e20b238e514bd644094283b6a328331ed64aad6e
Colin-techdev <clarencecolin@gmail.com>
2023-01-03 20:13:24 -0500

    updated password

---
+++ js/source.js
@@ -0,0 +1,27 @@
+
+
+
+
+function myFunction() {
+   let show = document.getElementById("hide-password");
+    if (show.type === "password") {
+      show.type = "text";
+    } else {
+      show.type = "password";
+    }
+  };
+
+
+  // function myFunction() {
+  //   let show = document.getElementById("hide-password");
+  //   if (show.type === "password") {
+  //     show.type = "text";
+  //   } else {
+  //     show.type = "password";
+  //   }
+  // }
+
+  // function myFunction() {
+  //   document.getElementById("myemail").required = true;
+  // }
+
